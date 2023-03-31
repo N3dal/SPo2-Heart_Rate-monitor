f9e3ee5398d5c5e0d1dff8d5aabfab4e1dd47585
N3dal <nedalxzo@gmail.com>
2023-03-31 18:18:37 +0200

    created clear function

--- utils.py
+++ utils.py
@@ -1,3 +1,37 @@
 """
     All tools needed for this project;
 """
+
+
+from os import name
+from os import system
+
+
+OS_NAME = name
+
+
+def clear():
+    """
+        :ARGS:
+            none;
+
+        :RETURNS:
+            return None;
+
+        :INFO:
+            wipe the terminal screen;
+    """
+
+    if OS_NAME == "posix":
+        # for *nix machines;
+        system("clear")
+
+    elif OS_NAME == "windows":
+        system("cls")
+
+    else:
+        # for any system in this world;
+        # system("your-clear-command-here")
+        pass
+
+    return None
