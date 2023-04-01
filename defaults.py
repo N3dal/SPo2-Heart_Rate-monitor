41fc7abdc0172f201cfc50bf3f2e83cd7013bb54
N3dal <nedalxzo@gmail.com>
2023-04-01 16:28:18 +0200

    updated defaults.py

--- defaults.py
+++ defaults.py
@@ -5,5 +5,5 @@
 
 class UI:
 
-    PLAY_PICTURE_PATH = r"./assets/play1.png"
-    STOP_PICTURE_PATH = r"./assets//stop1.png"
+    PLAY_PICTURE_PATH = r"./assets/play.png"
+    STOP_PICTURE_PATH = r"./assets//stop.png"
